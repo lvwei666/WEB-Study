794c41142ce1248295a96df877dc3b3232525aa0
lvwei666 <643398289@qq.com>
2019-08-02 11:42:33 +0800

    .

--- A-mySelf-study/test/index.js
+++ A-mySelf-study/test/index.js
@@ -1,11 +1,22 @@
-for(var i = 0; i < 10; i++) {
-  setTimeout(function() {
-    console.log(i);
-  },1000)
-}
+// for(var i = 0; i < 10; i++) {
+//   setTimeout(function() {
+//     console.log(i);
+//   },1000)
+// }
 
-for(let i = 0; i < 10; i++) {
-  setTimeout(function() {
-    console.log(i);
-  },1000)
+// for(let i = 0; i < 10; i++) {
+//   setTimeout(function() {
+//     console.log(i);
+//   },1000)
+// }
+
+// 如下：{1:222, 2:123, 5:888}，请把数据处理为如下结构：
+// [222, 123, null, null, 888, null, null, null, null, null, null, null]
+
+function objToArr(obj) {
+  let arr = [];
+  arr.length = 12;
+
+  return arr;
 }
+console.log(objToArr({1:222, 2:123, 5:888}))
